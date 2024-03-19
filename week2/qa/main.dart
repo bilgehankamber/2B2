3c6512e880388adbcaa5d55f39f813e44fd3053a
Bilgehan KAMBER <93771030+bilgehankamber@users.noreply.github.com>
2024-03-19 04:36:02 +0000

    exam qa

--- week2/qa/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:intro/homepage.dart';
-
-void main() {
-  runApp(MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Homepage()));
-}
